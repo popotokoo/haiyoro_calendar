381aa73633945f783707ca98f3c52d6917e22ad8
popotokoo <kirari.esp@gmail.com>
2026-06-09 19:37:55 +0900

    Point calendar config to Netlify proxy

--- haiyoro-village-calendar/config.js
+++ haiyoro-village-calendar/config.js
@@ -1,1 +1,1 @@
-window.HAIYORO_API_URL = "https://script.google.com/macros/s/AKfycbyhPGRwPceq-LxoEbkSmUeXig9L_SASE1FxzJgfP_x41ofwte9MeCab_i89n7ZYlDHu/exec";
+window.HAIYORO_API_URL = "https://haiyoro.netlify.app/.netlify/functions/calendar";
